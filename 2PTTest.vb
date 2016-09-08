38f767c386468fc6fad289f824af84b019998ca1
依瑪貓 <imacat@mail.imacat.idv.tw>
2016-09-08 17:09:02 +0800

    Prepended APLv2 license to the source.  Renamed 9Tools.vb to 9Utils.vb.  Added "DialogLibraries.loadLibrary" to load the dialogs first.

--- 2PTTest.vb
+++ 2PTTest.vb
@@ -1,3 +1,17 @@
+' Copyright (c) 2016 imacat.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law or agreed to in writing, software
+' distributed under the License is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+' See the License for the specific language governing permissions and
+' limitations under the License.
+
 ' 2PTTest: The macros to for generating the report of paired T-Test
 '   by imacat <imacat@mail.imacat.idv.tw>, 2016-08-11
 
@@ -9,6 +23,8 @@ Sub subRunPairedTTest As Object
 	Dim oSheets As Object, sSheetName As String
 	Dim oSheet As Object, mRanges As Object
 	Dim sExisted As String, nResult As Integer
+	
+	DialogLibraries.loadLibrary "Stats"
 	
 	' Asks the user for the data range
 	oRange = fnAskDataRange (ThisComponent)
